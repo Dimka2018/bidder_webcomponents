329da7061142383cdd18685bf027d34ddfdac86b
Dmitry_Plotnikov <dmitry_plotnikov@epam.com>
2020-08-13 11:19:33 +0300

    welcome page done

--- js/directive/WelcomeForm.js
+++ js/directive/WelcomeForm.js
@@ -7,31 +7,35 @@ class WelcomeForm extends HTMLElement {
         this.passwordPlaceholder = this.getAttribute('passwordPlaceholder');
         this.buttonText = this.getAttribute('buttonText');
         this.linkText = this.getAttribute('linkText');
+
+        this.linkEvent = this.getAttribute('linkEvent');
+        this.buttonEvent = this.getAttribute('buttonEvent');
     }
 
     connectedCallback() {
         this.innerHTML = this.getTemplate();
+        this.querySelector('.form_link').addEventListener('click', e => {
+            this.emmitEvent(this.linkEvent)
+        }, false);
+        this.querySelector('.form_button').addEventListener('click', e => {
+            e.preventDefault();
+            let login = this.querySelector('.login_input').value;
+            let password = this.querySelector('.password_input').value;
+            this.emmitEvent(this.buttonEvent, {login: login, password: password})
+        }, false);
     }
 
-    onLoginChange = event => {
-        this.setState({login: event.target.value});
-    };
-
-    onPasswordChange = event => {
-        this.setState({password: event.target.value});
-    };
-
-    onButtonClick = event => {
-        event.preventDefault();
-        this.props.callbacks.onButtonClick(this.state.login, this.state.password);
-    };
+    emmitEvent(eventName, data) {
+        const event = new CustomEvent(eventName, {bubbles: true, detail: data});
+        this.dispatchEvent(event);
+    }
 
     getTemplate() {
         return `
         <form class="user_form">
             <span class="form_header">${this.title}</span>
-            <input class="form_input" type='text' placeholder=${this.loginPlaceholder} />
-            <input class="form_input" type="password" placeholder=${this.passwordPlaceholder} />
+            <input class="form_input login_input" type='text' placeholder=${this.loginPlaceholder} />
+            <input class="form_input password_input" type="password" placeholder=${this.passwordPlaceholder} />
             <button class="form_button">${this.buttonText}</button>
             <a class="form_link">${this.linkText}</a>
         </form>
--- js/page/Welcome.js
+++ js/page/Welcome.js
@@ -2,25 +2,20 @@ class Welcome extends HTMLElement {
 
     constructor() {
         super();
-
-        this.error = '';
-        this.registrationMode = false;
-
         this.controller = new WelcomeController();
-
-        this.loginCallbacks = {
-            onButtonClick: this.login.bind(this)
-        };
-
-        this.registrationCallbacks = {
-            onButtonClick: this.register.bind(this),
-            onLinkClick: this.toggleMode.bind(this)
-        };
-
     }
 
     connectedCallback() {
         this.innerHTML = this.getTemplate();
+        this.addEventListener('toggle', e => {
+            this.toggleMode();
+        }, false);
+        this.addEventListener('login', e => {
+            this.login(e.detail.login, e.detail.password)
+        }, false);
+        this.addEventListener('register', e => {
+            this.register(e.detail.login, e.detail.password)
+        }, false)
     }
 
     toggleMode() {
@@ -31,23 +26,32 @@ class Welcome extends HTMLElement {
 
     login(login, password) {
         this.controller.login(login, password)
-            .then(() => this.props.history.push("/bid"),
-                error => this.setState({error: error}))
+            .then(() => this.emmitEvent('loggedIn'),
+                error => this.showError(error))
     }
 
     register(login, password) {
         this.controller.register(login, password)
             .then(() => this.login(login, password),
-                error => this.error = error)
+                error => this.showError(error))
+    }
+
+    showError(error) {
+        this.querySelector('.error_message').innerText = error;
+    }
+
+    emmitEvent(eventName, data) {
+        const event = new CustomEvent(eventName, {bubbles: true, detail: data});
+        this.dispatchEvent(event);
     }
 
     getTemplate() {
         return `
-        <div>
-            <welcome-form class="login_form" title="Log in" loginPlaceholder="login" passwordPlaceholder="password" buttonText="Log in" linkText="Registration"/>
-            <welcome-form class="registration_form none" title="Registration" loginPlaceholder="login" passwordPlaceholder="password" buttonText="Registrate" linkText="Log in"/>
-            <div class="error_message">${this.error || ''}</div>
-        </div>
+            <welcome-form class="login_form" title="Log in" loginPlaceholder="login" passwordPlaceholder="password" buttonText="Log in" linkText="Registration"
+            linkEvent="toggle" buttonEvent="login"></welcome-form>
+            <welcome-form class="registration_form none" title="Registration" loginPlaceholder="login" passwordPlaceholder="password" buttonText="Registrate" linkText="Log in"
+            linkEvent="toggle" buttonEvent="register"></welcome-form>
+            <div class="error_message"></div>
         `
     }
 }
